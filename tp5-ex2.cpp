bc0b432e4791cfb8dd3eb1675e9f7770b6fdd6b5
leticiarosa <38947588+leleh5@users.noreply.github.com>
2022-06-12 23:38:59 -0300

    Update tp5-ex2.cpp

--- tp5-ex2.cpp
+++ tp5-ex2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-#define MAX 5
+#define MAX 30
 
 struct No {
 	float dado;
@@ -30,20 +30,21 @@ int count(Pilha *p) {
 	return i;
 }
 
+int isEmpty(Pilha *p) {
+	return (p->topo == NULL);
+}
+
 int push(Pilha *p, float v) {
-	if((count(p)+1)<=MAX){
+	if(p->topo == NULL || p->topo->dado<v){
 		No *no = new No;
 		no->dado = v;
 		no->prox = p->topo;
 		p->topo = no;
 		return 1;
 	}
+
 	else{return 0;}
 
-}
-
-int isEmpty(Pilha *p) {
-	return (p->topo == NULL);
 }
 
 void print(Pilha *p) {
@@ -67,11 +68,11 @@ int main(int argc, char** argv)
 	cout << "Pilha vazia: " << (isEmpty(pilhaFloat)?"SIM":"NAO") << endl;
 
 	do{
-		cout << "Digite o número: " << endl;
+		cout << "Digite o n?mero: " << endl;
 		cin >> valor;
 		cond = push(pilhaFloat, valor);
 		cout << "Empilhado: " << (cond?"SIM":"NAO") << endl;
-	} while((count(pilhaFloat)+1)<MAX);
+	} while(count(pilhaFloat)<MAX);
 	
 	cout << "Qtde: " << count(pilhaFloat) << endl;
 
